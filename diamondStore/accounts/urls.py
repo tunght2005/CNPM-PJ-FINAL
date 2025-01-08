c7337d3bb6bba1790d32a3d80370dda47124a6b3
9hNek <bulletz6105@gmail.com>
2025-01-09 00:12:32 +0700

    Delete diamondStore directory, will reupload due to some error

--- diamondStore/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_out
-from .views import forgot
-
-urlpatterns = [
-    path('login-out/',login_out , name='login_out'),
-    path('forgot/',forgot, name='forgot'),
-]
