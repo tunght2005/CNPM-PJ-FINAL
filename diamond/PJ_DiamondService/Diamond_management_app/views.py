c844f4888c04594cc9a83e8d7fef14b5326cb206
Grek1785 <thanhdatya1785@gmail.com>
2025-01-09 02:26:23 +0700

    Update views.py

--- diamond/PJ_DiamondService/Diamond_management_app/views.py
+++ diamond/PJ_DiamondService/Diamond_management_app/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import diamond
 from django.http import HttpResponse, JsonResponse
 
-# View hiển thị danh sách diamonds
+
 def diamond_list(request):
     diamonds = diamond.objects.all()
     return render(request, 'table-data-info.html', {'diamonds': diamonds})
 
-# View thêm mới diamond
+
 def add_diamond(request):
     if request.method == 'POST':
-        # Lấy dữ liệu từ form
+       
         dia_name = request.POST['DiaName']
         carat = request.POST['carat']
         shape = request.POST['shape']
@@ -19,10 +19,10 @@ def add_diamond(request):
         clarity = request.POST['clarity']
         diamond_price = request.POST['DiamondPrice']
         
-        # Xử lý ảnh tải lên nếu có
+      
         image = request.FILES.get('image')
         
-        # Tạo và lưu vào cơ sở dữ liệu
+        
         diamond_obj = diamond(
             DiaName=dia_name,
             carat=carat,
@@ -39,28 +39,6 @@ def add_diamond(request):
     
     return render(request, 'form-add-news.html')
 
-# View chỉnh sửa diamond
-def edit_diamond(request, diamond_id):
-    diamond_obj = get_object_or_404(diamond, id=diamond_id)
-    
-    if request.method == 'POST':
-        diamond_obj.DiaName = request.POST['DiaName']
-        diamond_obj.carat = request.POST['carat']
-        diamond_obj.shape = request.POST['shape']
-        diamond_obj.cut = request.POST['cut']
-        diamond_obj.color = request.POST['color']
-        diamond_obj.clarity = request.POST['clarity']
-        diamond_obj.DiamondPrice = request.POST['DiamondPrice']
-        
-        # Cập nhật ảnh nếu có
-        if request.FILES.get('image'):
-            diamond_obj.image = request.FILES['image']
-        
-        diamond_obj.save()
-        
-        return redirect('diamond_list')  # Quay lại danh sách diamonds
-    
-    return render(request, 'form-edit-news.html', {'diamond': diamond_obj})
 
 def delete_diamond(request, diamond_id):
     if request.method == 'POST':
